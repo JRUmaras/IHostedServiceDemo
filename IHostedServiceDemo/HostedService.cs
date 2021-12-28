87923c898e59ad40cc4411b08473f94427ba32fe
Jonas Umaras <jonas.umaras@gmail.com>
2021-12-28 08:14:11 +0200

    Initial commit.

---
+++ IHostedServiceDemo/HostedService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace IHostedServiceDemo;
+
+public class HostedService : IHostedService, IAsyncDisposable
+{
+    private readonly ILogger<HostedService> _logger;
+    private int _executionCount;
+    private Timer? _timer;
+
+    public HostedService(ILogger<HostedService> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _executionCount = 0;
+        _timer = new Timer(DoWork, null, TimeSpan.FromSeconds(10), TimeSpan.FromSeconds(1));
+
+        _logger.LogInformation($"Started {nameof(HostedService)}...");
+
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation($"Stopping {nameof(HostedService)}...");
+
+        _timer?.Change(Timeout.Infinite, 0);
+
+        _logger.LogInformation($"Stopped {nameof(HostedService)}.");
+
+        return Task.CompletedTask;
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (_timer is IAsyncDisposable timer)
+        {
+            await timer.DisposeAsync();
+        }
+
+        _timer = null;
+    }
+
+    private void DoWork(object? state)
+    {
+        var count = Interlocked.Increment(ref _executionCount);
+
+        _logger.LogInformation("{Count}", count);
+    }
+}
---
+++ IHostedServiceDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using IHostedServiceDemo;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Topshelf;
+using Host = Microsoft.Extensions.Hosting.Host;
+
+Log.Logger = new LoggerConfiguration()
+    .CreateLogger();
+
+var rc = HostFactory.Run(x =>
+{
+    x.Service<IHost>(s =>
+    {
+        s.ConstructUsing(_ => Host
+            .CreateDefaultBuilder()
+            .ConfigureAppConfiguration(cb =>
+            {
+                cb.SetBasePath(Directory.GetCurrentDirectory());
+                cb.AddJsonFile("appsettings.json", false, true);
+            })
+            .ConfigureServices(sp =>
+            {
+                sp.AddHostedService<HostedService>(); 
+            })
+            .UseSerilog((hostingContext, loggerConfiguration) =>
+            {
+                loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration);
+            })
+            .Build());
+        s.WhenStarted(service => service.Start());
+        s.WhenStopped(service => service.StopAsync());
+    });
+
+    x.StartAutomatically();
+    x.RunAsLocalSystem();
+
+    x.SetServiceName("IHostedServiceDemo.ServiceName");
+    x.SetDisplayName("IHostedServiceDemo.DisplayName");
+    x.SetDescription("IHostedServiceDemo.Description");
+});
